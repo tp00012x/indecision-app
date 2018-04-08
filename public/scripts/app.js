e2a85bdfae4910bd90da1fa08eccf4079e698d98
Anthony Torres <atorres@g2crowd.com>
2018-04-08 12:20:56 -0500

    Finish section 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,6 +27,12 @@ var removeAll = function removeAll() {
     render();
 };
 
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var render = function render() {
@@ -49,9 +55,9 @@ var render = function render() {
             app.options.length > 0 ? 'Here are your options:' : 'No Options'
         ),
         React.createElement(
-            'p',
-            null,
-            app.options.length
+            'button',
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            'What should I do?'
         ),
         React.createElement(
             'button',
--- src/app.js
+++ src/app.js
@@ -25,6 +25,12 @@ const removeAll = () => {
     render();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
@@ -33,13 +39,11 @@ const render = () => {
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
-            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
             <button onClick={removeAll}>Remove All</button>
             <ol>
                 {
-                    app.options.map((option) => {
-                        return <li key={option}>{option}</li>
-                    })
+                    app.options.map((option) => <li key={option}>{option}</li>)
                 }
             </ol>
             <form onSubmit={onFormSubmit}>
